be6346eef4a38a39d608bfcef935ffe6a50ee7ce
sean lee <seanboramlee@gmail.com>
2020-04-04 19:53:40 -0700

    add quaranzine hero image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,36 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Issue 1: Handdrawn</h1>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const Home = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "quaranzine-hero.png" }) {
+        childImageSharp {
+          fluid {
+            base64
+            aspectRatio
+            src
+            srcSet
+            sizes
+          }
+        }
+      }
+    }
+  `)
 
-export default IndexPage
+  console.log("data", data)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Img fluid={data.file.childImageSharp.fluid} />
+
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
+
+export default Home
