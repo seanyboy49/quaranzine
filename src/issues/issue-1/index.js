a99306b72a2410004751a5d20986f2048c5835e4
Jina Zhu <jinazhu87@gmail.com>
2020-04-05 17:41:37 -0700

    created credit component

--- src/issues/issue-1/index.js
+++ src/issues/issue-1/index.js
@@ -3,11 +3,15 @@ import React from "react"
 import Cover from "./cover"
 import ApartmentDrawing from "./apartment-drawing"
 
+import Credits from "./credits"
+
 const Issue1 = () => {
   return (
     <>
       <Cover />
       <ApartmentDrawing />
+
+      <Credits />
     </>
   )
 }
