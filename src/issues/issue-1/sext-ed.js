498f4b875e2790f7298d7b7d9f22dffbd2072173
Jina Zhu <jinazhu87@gmail.com>
2020-04-06 22:06:50 -0700

    added guideline section

--- src/issues/issue-1/sext-ed.js
+++ src/issues/issue-1/sext-ed.js
@@ -4,6 +4,7 @@ import styled from "styled-components"
 import { useStaticQuery, graphql } from "gatsby"
 
 import { PaddedWidthContainer, Row, Column } from "../../styles/layout"
+import { Text, H2, H1 } from "../../styles/text"
 
 const ArtistNameContainer = styled.div`
   position: absolute;
@@ -14,7 +15,7 @@ const ArtistNameContainer = styled.div`
 `
 
 const ArtistName = styled.p`
-  font-size: 30px;
+  font-size: 23px;
 `
 
 const bulletpointStyle = styled.ul`
@@ -34,6 +35,9 @@ const Sext = () => {
           node {
             name
             childImageSharp {
+              fixed {
+                ...GatsbyImageSharpFixed_withWebp_tracedSVG
+              }
               fluid {
                 ...GatsbyImageSharpFluid_withWebp_tracedSVG
               }
@@ -43,6 +47,8 @@ const Sext = () => {
       }
     }
   `)
+
+  console.log(data)
 
   const imgTitle = data.allFile.edges.find(
     edge => edge.node.name === "Sext-Ed-101"
@@ -58,12 +64,20 @@ const Sext = () => {
     edge => edge.node.name === "1-digital-fucking"
   )
 
+  const phone = data.allFile.edges.find(edge => edge.node.name === "phone")
+
+  const communication = data.allFile.edges.find(
+    edge => edge.node.name === "2-good-communication"
+  )
+
+  const listen = data.allFile.edges.find(edge => edge.node.name === "listen")
+
   return (
     <PaddedWidthContainer id="sext-ed">
       <Img fluid={imgTitle.node.childImageSharp.fluid} />
-      <div style={{ position: "relative", marginTop: "10%" }}>
+      <div style={{ position: "relative", marginTop: "7%" }}>
         <Img
-          style={{ width: "60%", margin: "auto" }}
+          style={{ width: "50%", margin: "auto" }}
           fluid={imgFace.node.childImageSharp.fluid}
         />
         <ArtistNameContainer>
@@ -71,22 +85,24 @@ const Sext = () => {
         </ArtistNameContainer>
       </div>
       <Row>
-        <Column width="100" align="flex-start" style={{ margin: "0 17%" }}>
-          <h2>I love dirty talk. </h2>
-          <p>
+        <Column width="100" align="flex-start" style={{ margin: "0 23%" }}>
+          <H1>I love dirty talk. </H1>
+          <Text>
             And to be sexting—that is, sending sexually explicit photos, videos
             or text-based messages—or having phone sex during this period of
             self-isolation and quarantine feels especially connective. We’re
             limited but also enabled by our devices. We’re together, apart—and
             you know most of us (all of us?) are already masturbating.
-          </p>
-          <p>
+          </Text>
+          <Text>
             Staying home is an opportunity to turn inward: To work on ourselves,
             to self pleasure, and to improve specific communication skills to
             share with partners, if we so choose. Think of it as self love and
             collective love in the time of covid-19.
-          </p>
-          <p>Tl;dr: Now is a great time to brush up on your sex talk skills!</p>
+          </Text>
+          <Text>
+            Tl;dr: Now is a great time to brush up on your sex talk skills!
+          </Text>
         </Column>
       </Row>
       <Img
@@ -94,40 +110,72 @@ const Sext = () => {
         fluid={guidelines.node.childImageSharp.fluid}
       />
       <Img
-        style={{ width: "60%" }}
-        fluid={digitalFucking.node.childImageSharp.fluid}
+        style={{ width: "60%", height: "100px" }}
+        fixed={digitalFucking.node.childImageSharp.fixed}
+      />
+      <Row style={{ marginLeft: "5%" }}>
+        <Column width="60">
+          <Text>
+            Be upfront with your partners, just as you would IRL — and it
+            doesn’t have to be corny. I think it’s incredibly sexy for a new
+            partner to tell me where they would like to be touched. Or for
+            someone to set the vibe by asking to tell me about a specific
+            memory.
+          </Text>
+          <Text>
+            Maybe a new partner likes [redacted], but not [redacted] or their
+            [redacted] are especially sensitive. Maybe I’m at work right now and
+            can’t be distracted by play by play and pictures of your dick. Get
+            clear. Listen to each other; this is a dialogue, not a masturbatory
+            monologue.
+          </Text>
+          <Text>
+            Check in with partners on communications boundaries. You want to
+            know that any images or text messages you send are just for them
+            (unless part of y’all’s mutual agreement and interest is that they
+            share your sexts with other partners, etc. — and if so, you better
+            talk about it.)
+          </Text>
+        </Column>
+        <div style={{ width: "40%" }}>
+          <Img
+            style={{ width: "100%", margin: "10px" }}
+            fluid={phone.node.childImageSharp.fluid}
+          />
+        </div>
+      </Row>
+      <Img
+        style={{
+          width: "60%",
+          position: "relative",
+          marginLeft: "40%",
+          height: "60px",
+          marginTop: "20px",
+        }}
+        fixed={communication.node.childImageSharp.fixed}
       />
       <Row>
-        <Column>
-          <bulletpointStyle>
-            <li>
-              <p>
-                Be upfront with your partners, just as you would IRL — and it
-                doesn’t have to be corny. I think it’s incredibly sexy for a new
-                partner to tell me where they would like to be touched. Or for
-                someone to set the vibe by asking to tell me about a specific
-                memory.
-              </p>
-            </li>
-            <li>
-              <p>
-                Maybe a new partner likes [redacted], but not [redacted] or
-                their [redacted] are especially sensitive. Maybe I’m at work
-                right now and can’t be distracted by play by play and pictures
-                of your dick. Get clear. Listen to each other; this is a
-                dialogue, not a masturbatory monologue.
-              </p>
-            </li>
-            <li>
-              <p>
-                Check in with partners on communications boundaries. You want to
-                know that any images or text messages you send are just for them
-                (unless part of y’all’s mutual agreement and interest is that
-                they share your sexts with other partners, etc. — and if so, you
-                better talk about it.)
-              </p>
-            </li>
-          </bulletpointStyle>
+        <div style={{ width: "30%", marginLeft: "150px" }}>
+          <Img
+            style={{ width: "100%" }}
+            fluid={listen.node.childImageSharp.fluid}
+          />
+        </div>
+        <Column width="50">
+          <Text>
+            Tell your partner what you like / are into / want to do / want done
+            to you.
+          </Text>
+          <Text>
+            Ask your partner what they like / are into / want to do / want done
+            to them.
+          </Text>
+          <Text>
+            Listen to each other, with respect—and then respond. Do you have
+            different turn ons? Favorite positions? Do you have the same kink/s?
+            Are there new things you’d like to try together? Build excitement by
+            talking about them.
+          </Text>
         </Column>
       </Row>
     </PaddedWidthContainer>
--- src/styles/layout.js
+++ src/styles/layout.js
@@ -1,55 +1,60 @@
 import styled, { css } from "styled-components"
 
-export const PaddedWidthContainer = styled.section`
-  padding: 73.5px 145px;
+const breakpoints = {
+  phoneWide: `@media only screen and (max-width: 600px)`,
+  tabletWide: `@media only screen and (max-width: 880px)`,
+  laptop: `@media only screen and (max-width: 1024px)`,
+  desktop: `@media only screen and (max-width: 1440px)`,
+}
 
-  @media only screen and (max-width: 600px) {
-    padding: 28px 56px;
-  }
+export const PaddedWidthContainer = styled.section`
+  padding: 73.5px 145px;
+
+  ${breakpoints.phoneWide} {
+    padding: 28px 56px;
+  }
 `
 export const FullWidthContainer = styled.section`
-  padding: 145px 0;
+  padding: 145px 0;
 
-  @media only screen and (max-width: 600px) {
-    padding: 56px 0;
-  }
+  ${breakpoints.phoneWide} {
+    padding: 56px 0;
+  }
 `
 
 export const Row = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin: 20px, 0; 
 
-  ${p =>
-    p.center &&
-    css`
-      justify-content: center;
-    `};
+  ${breakpoints.tabletWide} {
+    flex-direction: column;
+    align-items: center;
+  }
 
-  ${p =>
-    p.start &&
-    css`
-      justify-content: flex-start;
-    `};
+  ${p =>
+  p.center &&
+  css`
+      justify-content: center;
+    `};
 
-  ${p =>
-    p.end &&
-    css`
-      justify-content: flex-end;
-    `};
+  ${p =>
+  p.start &&
+  css`
+      justify-content: flex-start;
+    `};
+
+  ${p =>
+  p.end &&
+  css`
+      justify-content: flex-end;
+    `};
 `
 
-export const Column = styled.div`
-  display: flex;
-  width: ${props => props.width || 50}%;
-  flex-direction: column;
-  align-items: center;
-`
-
-export const Row = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
+export const CustomPosition = styled.div`
+    position: absolute;
+    right: 0;
 `
 
 export const Column = styled.div`
@@ -57,4 +62,8 @@ export const Column = styled.div`
     width: ${props => props.width || 50}%;
     flex-direction: column;
     align-items: ${props => props.align || "center"};
+
+    ${breakpoints.tabletWide} {
+        width: 100%;
+      }
 `
