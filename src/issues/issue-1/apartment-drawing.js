76bfad5c971a048719d069916a52d6d478698071
sean lee <seanboramlee@gmail.com>
2020-04-09 22:34:35 -0700

    responsive apartment

--- src/issues/issue-1/apartment-drawing.js
+++ src/issues/issue-1/apartment-drawing.js
@@ -1,25 +1,40 @@
 import React from "react"
+import { useMediaQuery } from "react-responsive"
 import Img from "gatsby-image"
 import { useStaticQuery, graphql } from "gatsby"
 
-import { PaddedWidthContainer } from "../../styles/layout"
+import { findFileByName } from "../../utils"
+import { PaddedWidthContainer, breakpoints } from "../../styles/layout"
 
 const ApartmentDrawing = () => {
   const data = useStaticQuery(graphql`
     query {
-      file(relativePath: { eq: "apartment.png" }) {
-        childImageSharp {
-          fluid {
-            ...GatsbyImageSharpFluid_withWebp_tracedSVG
+      allFile(filter: { relativeDirectory: { eq: "apartment" } }) {
+        edges {
+          node {
+            name
+            childImageSharp {
+              fluid(quality: 100) {
+                ...GatsbyImageSharpFluid_withWebp_tracedSVG
+              }
+            }
           }
         }
       }
     }
   `)
 
+  const isPhoneWide = useMediaQuery({
+    query: breakpoints.phoneWide,
+  })
+
+  const desktopImage = findFileByName(data, "apartment-desktop")
+  const mobileImage = findFileByName(data, "apartment-mobile")
+  const imageToDisplay = isPhoneWide ? mobileImage : desktopImage
+
   return (
     <PaddedWidthContainer>
-      <Img fluid={data.file.childImageSharp.fluid} />
+      <Img fluid={imageToDisplay.node.childImageSharp.fluid} />
     </PaddedWidthContainer>
   )
 }
