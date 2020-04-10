c67eafa101f63972edb4c00176c0c5629926d175
sean lee <seanboramlee@gmail.com>
2020-04-09 22:31:42 -0700

    mobile responsvie nav bar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,28 @@
 import React from "react"
+import { useMediaQuery } from "react-responsive"
 
 import SEO from "../components/seo"
 import { NavBar, StyledLink } from "../styles/navigation"
 import { Text } from "../styles/text"
+import { breakpoints } from "../styles/layout"
 import Issue1 from "../issues/issue-1"
 import logo from "../images/logo.svg"
 
 const Home = () => {
+  const isPhoneWide = useMediaQuery({
+    query: breakpoints.phoneWide,
+  })
+
   return (
     <>
       <SEO title="Home" />
       <NavBar>
-        <img width={128} src={logo} alt="small qrzn logo"></img>
-        <Text center>the quaranzine for the quaranzined</Text>
+        <img
+          width={isPhoneWide ? 75 : 128}
+          src={logo}
+          alt="small qrzn logo"
+        ></img>
+        {!isPhoneWide && <Text center>the quaranzine for the quaranzined</Text>}
         <StyledLink to="/archive/">archive</StyledLink>
       </NavBar>
 
