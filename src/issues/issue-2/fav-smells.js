2c5fdbe7cc96071af18652eb2564d06670738134
Jina Zhu <jinazhu87@gmail.com>
2020-06-13 22:03:22 -0700

    start style carousel div

--- src/issues/issue-2/fav-smells.js
+++ src/issues/issue-2/fav-smells.js
@@ -1,21 +1,63 @@
 import React from "react"
 import { Carousel } from "react-responsive-carousel"
+import Img from "gatsby-image"
+import { useStaticQuery, graphql } from "gatsby"
+import { useMediaQuery } from "react-responsive"
+import styled from "styled-components"
+
+const ResizeImage = styled.div`
+  width: 500px;
+  height: 800px;
+  margin: 0;
+  padding: 0;
+`
 
 const FavoriteSmells = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allFile(
+        filter: { relativeDirectory: { eq: "issue2-images/fav-smell-images" } }
+      ) {
+        edges {
+          node {
+            name
+            childImageSharp {
+              fixed {
+                ...GatsbyImageSharpFixed_withWebp_tracedSVG
+              }
+              fluid {
+                ...GatsbyImageSharpFluid_withWebp_tracedSVG
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  const afterShowerImg = data.allFile.edges.find(
+    edge => edge.node.name === "after-shower"
+  )
+  const boulangerieImg = data.allFile.edges.find(
+    edge => edge.node.name === "boulangerie"
+  )
   return (
     <Carousel autoPlay>
-      <div>
-        <div>
-          <img
-            alt=""
-            src="http://lorempixel.com/output/cats-q-c-640-480-1.jpg"
-            width="100"
-          />
-          <p className="legend">Legend 1</p>
-        </div>
+      <div
+        style={{
+          background: "#d6f2f2",
+          width: "100%",
+          height: "50%",
+          position: "fixed",
+          top: "0",
+          left: "0",
+        }}
+      >
+        <ResizeImage>
+          <Img fluid={afterShowerImg.node.childImageSharp.fluid} />
+        </ResizeImage>
       </div>
       <div>
-        <img alt="" src="http://lorempixel.com/output/cats-q-c-640-480-2.jpg" />
+        <Img fluid={boulangerieImg.node.childImageSharp.fluid} />
         <p className="legend">Legend 2</p>
       </div>
     </Carousel>
