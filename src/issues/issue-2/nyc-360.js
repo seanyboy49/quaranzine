dc315a00e1f44ae8fc5bba444ddaa0148947ffe5
Jina Zhu <jinazhu87@gmail.com>
2020-06-24 21:55:27 -0700

    structure nyc page

---
+++ src/issues/issue-2/nyc-360.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Img from "gatsby-image"
+import { useStaticQuery, graphql } from "gatsby"
+
+import { findFileByName } from "../../utils"
+
+const Nyc360 = () => {
+    const data = useStaticQuery(graphql`
+    query {
+      allFile(
+        filter: { relativeDirectory: { eq: "issue2-images" } }
+      ) {
+        edges {
+          node {
+            name
+            childImageSharp {
+              fixed {
+                ...GatsbyImageSharpFixed_withWebp_tracedSVG
+              }
+              fluid {
+                ...GatsbyImageSharpFluid_withWebp_tracedSVG
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const roofTop = findFileByName(data, "my-rooftop-quarantine")
+return (
+
+)
+}
+
+export default Nyc360
