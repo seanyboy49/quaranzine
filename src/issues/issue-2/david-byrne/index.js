8bc30c545fd8183a3de9ad212a8131a0ce93f733
sean lee <seanboramlee@gmail.com>
2020-06-25 21:36:00 -0700

    transform func to map images to slide data

--- src/issues/issue-2/david-byrne/index.js
+++ src/issues/issue-2/david-byrne/index.js
@@ -3,11 +3,11 @@ import Img from "gatsby-image"
 import { useStaticQuery, graphql } from "gatsby"
 
 import { Background } from "./styled"
-
 import { PaddedWidthContainer, breakpoints } from "../../../styles/layout"
+import slides from "./slides"
 
 const DavidByrne = () => {
-  const data = useStaticQuery(graphql`
+  const { mini, big } = useStaticQuery(graphql`
     query {
       mini: allFile(
         filter: { relativeDirectory: { eq: "issue-2/david-byrne/mini" } }
@@ -21,7 +21,20 @@ const DavidByrne = () => {
       }
     }
   `)
-  console.log("data", data)
+
+  const slidesWithImages = slides.map(slide => {
+    const bigImg = big.nodes.find(node => node.name === slide.year)
+    const miniImg = mini.nodes.find(node => node.name === slide.year)
+
+    return {
+      ...slide,
+      bigImg,
+      miniImg,
+    }
+  })
+
+  console.log("slidesWithImages", slidesWithImages)
+
   return (
     <Background backgroundColor="blue">
       <h1>David Byrne</h1>
--- src/issues/issue-2/david-byrne/types.js
+++ src/issues/issue-2/david-byrne/types.js
@@ -3,13 +3,11 @@ import { graphql } from "gatsby"
 
 export const imageFragment = graphql`
   fragment ImageFragment on FileConnection {
-    edges {
-      node {
-        name
-        childImageSharp {
-          fluid(quality: 100) {
-            ...GatsbyImageSharpFluid_withWebp_tracedSVG
-          }
+    nodes {
+      name
+      childImageSharp {
+        fluid(quality: 100) {
+          ...GatsbyImageSharpFluid_withWebp_tracedSVG
         }
       }
     }
