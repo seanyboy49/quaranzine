bc2ba09f89623faf3b272ba0ad0a0bf5f27e1449
Jina Zhu <jinazhu87@gmail.com>
2020-06-27 21:36:03 -0700

    removed image to fix bug

--- src/issues/issue-2/nyc-360.js
+++ src/issues/issue-2/nyc-360.js
@@ -5,32 +5,28 @@ import { useStaticQuery, graphql } from "gatsby"
 import { findFileByName } from "../../utils"
 
 const Nyc360 = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      allFile(filter: { relativeDirectory: { eq: "issue2-images" } }) {
-        edges {
-          node {
-            name
-            childImageSharp {
-              fixed {
-                ...GatsbyImageSharpFixed_withWebp_tracedSVG
-              }
-              fluid {
-                ...GatsbyImageSharpFluid_withWebp_tracedSVG
-              }
-            }
-          }
-        }
-      }
-    }
-  `)
+  // const data = useStaticQuery(graphql`
+  //   query {
+  //     allFile(filter: { relativeDirectory: { eq: "issue2-images" } }) {
+  //       edges {
+  //         node {
+  //           name
+  //           childImageSharp {
+  //             fixed {
+  //               ...GatsbyImageSharpFixed_withWebp_tracedSVG
+  //             }
+  //             fluid {
+  //               ...GatsbyImageSharpFluid_withWebp_tracedSVG
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
 
-  const roofTop = findFileByName(data, "my-rooftop-quarantine")
-  return (
-    <div>
-      <Img fluid={roofTop.node.childImageSharp.fluid} />
-    </div>
-  )
+  // const roofTop = findFileByName(data, "my-rooftop-quarantine")
+  return <div>{/* <Img fluid={roofTop.node.childImageSharp.fluid} /> */}</div>
 }
 
 export default Nyc360
