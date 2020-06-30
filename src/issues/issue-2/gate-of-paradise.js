d738364332f121fa08e523b5b169d5863631a5eb
Jina Zhu <jinazhu87@gmail.com>
2020-06-29 21:20:28 -0700

    add street section

--- src/issues/issue-2/gate-of-paradise.js
+++ src/issues/issue-2/gate-of-paradise.js
@@ -9,7 +9,9 @@ const Paradise = () => {
   const data = useStaticQuery(graphql`
     query {
       allFile(
-        filter: { relativeDirectory: { eq: "issue2-images/gate-of-paradise" } }
+        filter: {
+          relativeDirectory: { eq: "issue2-images/gate-of-paradise-images" }
+        }
       ) {
         edges {
           node {
@@ -31,7 +33,44 @@ const Paradise = () => {
   const park_img = findFileByName(data, "park-edit")
   const sidewalk_img = findFileByName(data, "sidewalk-edit")
   const street_img = findFileByName(data, "street-edit")
-  return <></>
+  return (
+    <>
+      <div>
+        <Img fixed={sidewalk_img.node.childImageSharp.fixed} />
+        <p>
+          It's a sin to seek perfection
+          <br />
+          It's a sin to help the poor
+          <br />
+          It's a sin to hold convictions
+          <br />
+          For none of them are true
+          <br />
+          <br />
+          And the laws of men are not the laws of heaven
+          <br />
+          And angel's breath is like the desert wind
+          <br />
+          And terrorists are acting out of love, sweet love
+          <br />
+          To bring us home again
+          <br />
+          <br />
+          Beware of good intentions
+          <br />
+          And the passion in their eyes
+          <br />
+          For none of them can open
+          <br />
+          The gates of paradise
+          <br />
+        </p>
+      </div>
+
+      {/* <Img fixed={park_img.node.childImageSharp.fixed} /> */}
+      {/* <Img fixed={street_img.node.childImageSharp.fixed} /> */}
+    </>
+  )
 }
 
 export default Paradise
--- src/issues/issue-2/index.js
+++ src/issues/issue-2/index.js
@@ -3,10 +3,12 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"
 
 import FavoriteSmells from "./fav-smells"
 import Nyc360 from "./nyc-360"
+import Paradise from "./gate-of-paradise"
 
 const Issue2 = () => {
   return (
     <>
+      <Paradise />
       <FavoriteSmells />
       <Nyc360 />
     </>
