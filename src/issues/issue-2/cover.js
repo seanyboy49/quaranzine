dded1fd93aae20fdc76efb64544af34ef2db417e
Jina Zhu <jinazhu87@gmail.com>
2020-07-04 21:55:35 -0700

    add cover and issue number

--- src/issues/issue-2/cover.js
+++ src/issues/issue-2/cover.js
@@ -1,30 +1,54 @@
 import React from "react"
 import Img from "gatsby-image"
+import { useStaticQuery, graphql } from "gatsby"
 import { findFileByName } from "../../utils"
+import styled from "styled-components"
+import { mediaQueries, breakpoints } from "../../styles/layout"
+
+const IssueWrapper = styled.div`
+  background-color: #ff7b4d;
+  margin: 0;
+  padding: 2rem;
+  display: flex;
+  justify-content: flex-end;
+
+  ${mediaQueries.tabletWide} {
+    padding: 1rem;
+  }
+`
+
+const IssueNum = styled.h3`
+  font-size: 3rem;
+  color: white;
+  margin: 0;
+  padding: 0;
+
+  ${mediaQueries.tabletWide} {
+    font-size: 2rem;
+  }
+`
 
 const Cover = () => {
-  const data = useStaticQuery(graphql`
+  const coverImage = useStaticQuery(graphql`
     query {
-      allFile(filter: { relativeDirectory: { eq: "issue2-images" } }) {
-        edges {
-          node {
-            name
-            childImageSharp {
-              fluid(quality: 100) {
-                ...GatsbyImageSharpFluid_withWebp_tracedSVG
-              }
-            }
+      file(relativePath: { eq: "issue2-images/issue2-cover.png" }) {
+        childImageSharp {
+          fluid(quality: 100) {
+            ...GatsbyImageSharpFluid_withWebp_tracedSVG
           }
         }
       }
     }
   `)
 
-  const CoverImage = findFileByName(data, "issue2-cover")
+  console.log("coverImage", coverImage)
 
   return (
     <>
-      <Img fluid={CoverImage.node.childImageSharp.fluid} />
+      <Img fluid={coverImage.file.childImageSharp.fluid} />
+      <IssueWrapper>
+        <IssueNum>issue #2: collage</IssueNum>
+      </IssueWrapper>
     </>
   )
 }
